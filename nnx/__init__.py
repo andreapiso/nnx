fa5ebb8d1ff1b024e01f094ba3e8bbdd72994365
andreapiso <andreapiso@gmail.com>
2019-12-08 22:35:39 +0800

    Add requirements.

---
+++ nnx/__init__.py
@@ -0,0 +1,8 @@
+import sys
+if sys.version_info[:2] < (3, 6):
+    m = "Python 3.6 or later is required for nnx (%d.%d detected)."
+    raise ImportError(m % sys.version_info[:2])
+del sys
+
+import nnx.classes
+from nnx.classes import *
--- nnx/classes/tests/test_graph.py
+++ nnx/classes/tests/test_graph.py
@@ -2,3 +2,8 @@ from numba import njit
 from numba import int64
 from numba.tests.support import TestCase, MemoryLeakMixin, unittest
 
+from test_support_base_data import create_complete_graph_edge_list
+
+class TestCreateAddEdgeLength(MemoryLeakMixin, TestCase):
+    '''Test graph creation, add edge and length'''
+    
